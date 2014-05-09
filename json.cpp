394e1aee9e8b1de7991fd3270b0ec3193a4b0543
wuhao <gfphoenix@qq.com>
2014-05-10 01:43:55 +0800

    a simple json parser using flex++bison, C++ some data format is not implemented yet

---
+++ json.cpp
@@ -0,0 +1,121 @@
+#include "json.h"
+#include <stdio.h>
+#include <string.h>
+
+using namespace Json;
+
+bool BoolValue::Encode(const std::function<int (const void *, unsigned)>& writer)const
+{
+#define     _J_TRUE    "true"
+#define     _J_FALSE    "false"
+    int ret;
+    if(_bool){
+        ret = writer(_J_TRUE , sizeof(_J_TRUE)-1);
+        return ret == sizeof(_J_TRUE)-1;
+    }
+    ret = writer(_J_FALSE , sizeof(_J_FALSE)-1);
+    return ret == sizeof(_J_FALSE)-1;
+#undef      _J_TRUE
+#undef      _J_FALSE
+}
+bool IntValue::Encode(const std::function<int (const void *, unsigned)>& writer)const
+{
+    char buf[64];
+    int n = snprintf(buf, sizeof(buf), "%ld", _v);
+    return writer(buf, strlen(buf)) == n;
+}
+bool FloatValue::Encode(const std::function<int (const void *, unsigned)>& writer)const
+{
+    char buf[64];
+    int n = snprintf(buf, sizeof(buf), "%lf", _v);
+    return writer(buf, strlen(buf)) == n;
+}
+bool StringValue::Encode(const std::function<int (const void *, unsigned)>& writer)const
+{
+    return writer(_v.c_str(), (unsigned)_v.size()) == (int)_v.size();
+}
+bool ArrayValue::Encode(const std::function<int (const void *, unsigned)>& writer)const 
+{
+    auto sz = _v.size();
+    int e;
+    e = writer("[", 1);
+    if(e != 1)
+        return false;
+    if(sz == 0)
+        goto out;
+
+    if(! _v[0]->Encode(writer))
+        return false;
+    for(auto x = _v.cbegin()+1; x != _v.cend(); x++){
+        if(writer(",", 1) != 1)
+            return false;
+        if(!(*x)->Encode(writer))
+            return false;
+    }
+out:
+    e = writer("]", 1);
+    if(e != 1)
+        return false;
+    return true;
+}
+
+bool ObjectValue::Encode(const std::function<int (const void *, unsigned)>& writer)const
+{
+    int e;
+    e = writer("{\n", 2);
+    if(e != 2)
+        return false;
+    if(m.size() == 0)
+        goto out;
+
+    for(auto x = m.cbegin(); x != m.cend(); x++){
+        auto &k = x->first;
+        auto &v = x->second;
+        char buf[(unsigned)k.size() + 16];
+        int n = snprintf(buf, (unsigned)k.size()+16, "\"%s\" : ", k.c_str());
+        e = writer(buf, n);
+        if(e!=n || e != (int)k.size()+5)
+            return false;
+        auto ok = v->Encode(writer);
+        if(!ok)
+            return false;
+        e = writer(",\n", 2);
+        if(e != 2)
+            return false;
+    }
+out:
+    e = writer("}", 1);
+    if(e != 1)
+        return false;
+    return true;
+}
+
+// ==============================================================================================
+// ############## end of encode
+
+#if 0
+//fix me
+StringValue::StringValue(std::string &&v)
+    :Value(Type::jType_STRING), _v(std::move(v)){}
+ArrayValue::ArrayValue(const std::vector<Value*> &v)
+    :Value(Type::jType_ARRAY)
+{
+    _v = v;
+}
+//move constructor
+ArrayValue::ArrayValue(std::vector<Value*> &&v)
+    :Value(Type::jType_ARRAY)
+{
+    _v = v;
+}
+
+#endif
+
+
+
+
+
+
+
+
+
---
+++ json.h
@@ -0,0 +1,140 @@
+#ifndef _MYJSON_H
+#define _MYJSON_H
+
+#include <functional>
+#include <string>
+#include <vector>
+#include <map>
+
+namespace Json {
+    enum class Type {
+        jType_UNKNOWN = 1,
+        jType_OBJECT,
+        jType_ARRAY,
+        jType_BOOL,
+        jType_INT,
+        jType_FLOAT,
+        jType_STRING,
+    };
+    class Value {
+        public:
+            Type type;
+            Value(Type t):type(t){}
+            virtual ~Value(){type = Type::jType_UNKNOWN;}
+            virtual bool Encode(const std::function<int (const void *, unsigned)>& writer)const = 0;
+
+            //check type
+            bool isIntType()const {return type == Type::jType_INT;}
+            bool isFloatType()const {return type == Type::jType_FLOAT;}
+            bool isBoolType()const {return type == Type::jType_BOOL;}
+            bool isStringType()const {return type == Type::jType_STRING;}
+            bool isArrayType()const {return type == Type::jType_ARRAY;}
+            bool isObjectType()const {return type == Type::jType_OBJECT;}
+    };
+    class BoolValue : public Value {
+        private:
+            bool _bool;
+        public:
+            BoolValue(bool b):Value(Type::jType_BOOL), _bool(b){}
+            bool getValue()const{return _bool;}
+            void setValue(bool b){_bool = b;}
+
+        protected:
+            bool Encode(const std::function<int (const void *, unsigned)>& writer)const ;
+    };
+    class IntValue : public Value {
+        private:
+            long _v;
+        public:
+            IntValue(long v):Value(Type::jType_INT), _v(v){}
+            long getValue()const {return _v;}
+            void setValue(long v){_v = v;}
+        protected:
+            bool Encode(const std::function<int (const void *, unsigned)>& writer)const ;
+    };
+    class FloatValue : public Value {
+        private:
+            double _v;
+        public:
+            FloatValue(double v):Value(Type::jType_FLOAT), _v(v){}
+            double getValue()const {return _v;}
+            void setValue(double v){_v = v;}
+        protected:
+            bool Encode(const std::function<int (const void *, unsigned)>& writer)const ;
+    };
+    class StringValue : public Value {
+        private:
+            std::string _v;
+        protected:
+            bool Encode(const std::function<int (const void *, unsigned)>& writer)const ;
+        public:
+            StringValue(const std::string &v):Value(Type::jType_STRING), _v(v){}
+            //            StringValue(std::string &&v);
+            std::string getValue()const {return _v;}
+            void setValue(const std::string &v){ _v = v;}
+            //           void setValue(std::string && v){_v = std::move(v);}
+
+    };
+    class ArrayValue : public Value {
+        private:
+            std::vector<Value*> _v;
+        public:
+            //          ArrayValue(Value * v, ...);
+            ArrayValue(const std::vector<Value*> &v):Value(Type::jType_ARRAY),_v(v){}
+            //         ArrayValue(std::vector<Value*> &&v);
+            Value *operator[](int idx) {
+                if(idx<0 || idx>=_v.size())
+                    return nullptr;
+                return _v[idx];
+            }
+            //add iterator
+        protected:
+            bool Encode(const std::function<int (const void *, unsigned)>& writer)const ;
+    };
+    class ObjectValue : public Value {
+        private:
+            std::map<std::string, Value*> m;
+        public:
+            ObjectValue():Value(Type::jType_OBJECT){}
+            ObjectValue(const std::map<std::string, Value *> &map):Value(Type::jType_OBJECT),m(map){}
+            Value * operator[](const std::string &key){return m[key];}
+
+            bool hasKey(const std::string &k){
+                return m[k] != nullptr;
+            }
+
+            bool Encode(const std::function<int (const void *, unsigned)>& writer)const ;
+    };
+
+    namespace {
+        int defaultReader(void *buf, unsigned n)
+        {
+            return 0;
+        }
+        int defaultWriter(const void *buf, unsigned n)
+        {
+            printf("%s", buf);
+            return n;
+        }
+    }
+
+    class JsonDef {
+        public:
+            std::function<int (void *, unsigned)> R;
+            std::function<int (const void *, unsigned)> W;
+    };
+
+    class Json {
+        private:
+            std::function<int (void *, unsigned)> reader;
+            std::function<int (const void *, unsigned)> writer;
+            ObjectValue *obj;
+        public:
+            Json(const JsonDef &def);
+            //Json(const std::function<int(void *, unsigned)> &r = defaultReader,
+            //        const std::function<int(const void *, unsigned)> &w=defaultWriter);
+            ObjectValue *Decode()const;
+            bool Encode()const{return obj->Encode(writer);}
+    };
+}
+#endif
